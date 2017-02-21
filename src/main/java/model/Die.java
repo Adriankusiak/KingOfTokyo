7c7c716123f207f6b38b9ece9d84bcdc1be4dbf2
Adrian Kusiak <adrian.kusiak@kcl.ac.uk>
2017-02-21 13:44:58 +0000

    Refactor Dice class to represent single die

---
+++ src/main/java/model/Die.java
@@ -0,0 +1,40 @@
+package model;
+
+import java.util.Random;
+
+public class Die {
+
+    private static Random generator = new Random();
+    private int value;
+    private boolean rollable;
+    private int rolls;
+
+    public Die(int rolls){
+        value = 0;
+        rollable=true;
+        this.rolls = rolls;
+    }
+
+    public int roll(){
+        rolls -= 1;
+        value = generator.nextInt(6) + 1;
+        checkRollable();
+        return value;
+    }
+
+    private void checkRollable() {
+        rollable = (rolls==0)?false:true;
+    }
+
+    public void addRoll(){
+        rolls+=1;
+        checkRollable();
+    }
+    public void setRolls(int val){
+        rolls = val;
+        checkRollable();
+    }
+
+    public boolean isRollable() {return rollable;}
+    public int getValue(){return value;}
+}
